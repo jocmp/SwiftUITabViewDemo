1fcba2ef70a940d2eb48146a9d8bee76559a3a5c
Josiah Campbell <jocmp64@gmail.com>
2021-05-12 19:10:31 -0400

    Initial Commit

---
+++ TabDemo/TabDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabDemoApp.swift
+//  TabDemo
+//
+//  Created by Josiah Campbell on 5/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TabDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
