b8488b3a8197f85a4f740759aa04e61ca9a539c7
Josiah Campbell <jocmp64@gmail.com>
2021-05-12 19:22:23 -0400

    Add tabview with two tabs

--- TabDemo/ContentView.swift
+++ TabDemo/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 1
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selection) {
+            VStack {
+                Text("Hello 1")
+            }
+            .tabItem {
+                Label("Tab 1", systemImage: selection == 1 ? "star.fill" : "star")
+            }
+            .tag(1)
+            NavigationView {
+                VStack {
+                    NavigationLink(destination: DetailView()) {
+                        Text("Hello 2")
+                    }
+                    .navigationTitle("Index")
+                }
+            }
+            .tabItem {
+                Label("Tab 2", systemImage: selection == 2 ? "moon.fill" : "moon")
+            }
+            .tag(2)
+        }
+    }
+}
+
+struct DetailView: View {
+    var body: some View {
+        VStack {
+            Text("Hello child")
+        }
+        .navigationTitle("Detail")
     }
 }
 
